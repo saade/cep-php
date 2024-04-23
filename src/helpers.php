027b41960b7de3968a4f4dbef02a73ec3c74ce5a
saade <saade@users.noreply.github.com>
2024-04-23 14:53:14 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -7,7 +7,7 @@ function sanitizeCEP(string $cep, bool $formatted = false): string
     $cep = preg_replace('/[^0-9]/', '', $cep);
     $cep = str_pad($cep, 8, '0', STR_PAD_LEFT);
 
-    if($formatted) {
+    if ($formatted) {
         $cep = preg_replace('/^(\d{5})(\d{3})$/', '$1-$2', $cep);
     }
 
